107520dd286c9dae8802c0fe97af338eac8c2535
Alin Panaitiu <alin.p32@gmail.com>
2017-11-26 11:03:33 +0200

    Fix related artists

--- spfy/mixins/recommender.py
+++ spfy/mixins/recommender.py
@@ -83,7 +83,7 @@ class RecommenderMixin:
                 if a.id not in disliked_artists}
 
             related_artists_limit = random.randint(1, limit - len(artist_set))
-            artist_set |= {a.id for a in random.sample(related_artists.artists, related_artists_limit)}
+            artist_set |= random.sample(related_artists, related_artists_limit)
 
         return artist_set
 
