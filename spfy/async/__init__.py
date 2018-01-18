2c40c896c9d8064d4301534a5db6080890c013fa
Alin Panaitiu <alin.p32@gmail.com>
2018-01-18 16:33:40 +0200

    Make sure we have access to every sync module in async

--- spfy/async/__init__.py
+++ spfy/async/__init__.py
@@ -1,3 +1,5 @@
+from .. import *  # isort:skip
+
 import asyncio
 from itertools import islice
 
--- spfy/mixins/async/__init__.py
+++ spfy/mixins/async/__init__.py
@@ -1,3 +1,5 @@
+from .. import *  # isort:skip
+
 from .auth import AuthMixin  # noqa
 from .player import PlayerMixin  # noqa
 from .recommender import RecommenderMixin  # noqa
