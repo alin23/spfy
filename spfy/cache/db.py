1df7d1727f3265ae1a3b21c0fe87ab7a2ac2772a
Alin Panaitiu <alin.p32@gmail.com>
2018-01-24 19:08:42 +0200

    Separate image fetching logic inside a mixin, add Country and City

--- spfy/cache/db.py
+++ spfy/cache/db.py
@@ -10,6 +10,7 @@ from datetime import date, datetime
 import aiohttp
 import requests
 import psycopg2.extras
+from first import first
 # pylint: disable=unused-import
 from pony.orm import (
     Set,
@@ -27,6 +28,7 @@ from pony.orm import (
     db_session,
     composite_key
 )
+from pycountry import countries
 from colorthief import ColorThief
 from psycopg2.extensions import register_adapter
 
@@ -41,6 +43,55 @@ if os.getenv('SQL_DEBUG'):
     logging.getLogger('pony.orm.sql').setLevel(logging.DEBUG)
 
 db = Database()
+
+
+class ImageMixin:
+    def image(self, width=None, height=None):
+        if width:
+            image = self.images.select().where(lambda i: i.width >= width).order_by(Image.width).first()
+        elif height:
+            image = self.images.select().where(lambda i: i.height >= height).order_by(Image.height).first()
+        else:
+            image = self.images.select().order_by(desc(Image.width)).first()
+
+        return image
+
+    def get_image_queries(self):
+        words = self.name.split()
+        stems = [[w[:i] for i in range(len(w), 2, -1)] for w in words]
+        queries = [*words, self.name, *sum(stems, [])]
+        return [f'{query} music' for query in queries]
+
+    async def fetch_unsplash_image(self, width=None, height=None):
+        image = self.image(width, height)
+        if image:
+            return image
+
+        queries = self.get_image_queries()
+        photo = None
+        for query in queries:
+            photos = await Unsplash.photo.random(query=query, orientation='squarish')
+            if photos:
+                photo = photos[0]
+                break
+        else:
+            photo = await Unsplash.photo.random(query=query, orientation='squarish')[0]
+
+        if photo is None:
+            return None
+
+        ratio = photo.height / photo.width
+        params = {
+            'genre': self,
+            'color': photo.color,
+            'unsplash_user_fullname': photo.user.name,
+            'unsplash_user_username': photo.user.username,
+        }
+        Image(url=photo.urls.full, width=photo.width, height=photo.height, **params)
+        Image(url=photo.urls.regular, width=Image.REGULAR, height=int(round(ratio * Image.REGULAR)), **params)
+        Image(url=photo.urls.small, width=Image.SMALL, height=int(round(ratio * Image.SMALL)), **params)
+        Image(url=photo.urls.thumb, width=Image.THUMB, height=int(round(ratio * Image.THUMB)), **params)
+        return self.image(width, height)
 
 
 class User(db.Entity):
@@ -64,10 +115,16 @@ class User(db.Entity):
     top_genres = Set('Genre')
     disliked_genres = Set('Genre')
 
+    top_countries = Set('Country')
+    disliked_countries = Set('Country')
+
+    top_cities = Set('City')
+    disliked_cities = Set('City')
+
     top_expires_at = Optional(Json, volatile=True)
 
-    def dislike(self, artist=None, genre=None):
-        assert artist or genre
+    def dislike(self, artist=None, genre=None, country=None, city=None):
+        assert artist or genre or country or city
         if artist:
             self.disliked_artists.add(artist)
             self.top_artists.remove(artist)
@@ -76,6 +133,12 @@ class User(db.Entity):
             self.top_genres.remove(genre)
             self.disliked_artists.add(genre.artists)
             self.top_artists.remove(genre.artists)
+        if country:
+            self.disliked_countries.add(country)
+            self.top_countries.remove(country)
+        if city:
+            self.disliked_cities.add(city)
+            self.top_cities.remove(city)
 
     def top_expired(self, time_range):
         time_range = TimeRange(time_range).value
@@ -114,6 +177,8 @@ class Image(db.Entity):
     playlist = Optional('Playlist')
     artist = Optional('Artist')
     genre = Optional('Genre')
+    country = Optional('Country')
+    city = Optional('City')
     unsplash_user_fullname = Optional(str)
     unsplash_user_username = Optional(str)
 
@@ -167,71 +232,95 @@ class SpotifyUser(db.Entity):
         return f'http://open.spotify.com/user/{self.id}'
 
 
-class Genre(db.Entity):
+class Genre(db.Entity, ImageMixin):
     _table_ = 'genres'
 
     name = PrimaryKey(str)
-    images = Set(Image)
     artists = Set('Artist')
     playlists = Set('Playlist')
     fans = Set(User, reverse='top_genres', table='genre_fans')
     haters = Set(User, reverse='disliked_genres', table='genre_haters')
+    images = Set(Image, cascade_delete=True)
 
     def play(self, client, device=None):
         popularity = random.choice(list(Playlist.Popularity)[:3])
         playlist = client.genre_playlist(self.name, popularity)
         return playlist.play(device=device)
 
-    def image(self, width=None, height=None):
-        if width:
-            image = self.images.select().where(lambda i: i.width >= width).order_by(Image.width).first()
-        elif height:
-            image = self.images.select().where(lambda i: i.height >= height).order_by(Image.height).first()
-        else:
-            image = self.images.select().order_by(desc(Image.width)).first()
 
-        return image
+class Country(db.Entity, ImageMixin):
+    _table_ = 'countries'
+
+    name = PrimaryKey(str)
+    code = Required(str, index=True, max_len=2)
+    cities = Set('City')
+    playlists = Set('Playlist')
+    fans = Set(User, reverse='top_countries', table='country_fans')
+    haters = Set(User, reverse='disliked_countries', table='country_haters')
+    images = Set(Image, cascade_delete=True)
+
+    @staticmethod
+    def _name_match(country, name):
+        try:
+            return name in country.name.lower() or name in country.official_name.lower()
+        except:
+            return False
+
+    @classmethod
+    def from_str(cls, name=None, code=None):
+
+        iso_country = None
+        if name == 'UK':
+            iso_country = countries.get(alpha_2='GB')
+        elif name == 'USA':
+            iso_country = countries.get(alpha_2='US')
+        elif name is not None:
+            try:
+                iso_country = countries.get(name=name)
+            except KeyError:
+                try:
+                    iso_country = countries.get(official_name=name)
+                except KeyError:
+                    iso_country = first(countries, key=lambda c: cls._name_match(c, name.lower()))
+        elif code is not None:
+            try:
+                iso_country = countries.get(alpha_2=code)
+            except KeyError:
+                iso_country = first(countries, key=lambda c: code.lower() in c.alpha_2.lower())
+
+        if not iso_country:
+            logger.error(f'Could not find a country with name={name} and code={code}')
+            return None
+
+        return cls.get(name=iso_country.name) or cls(name=iso_country.name, code=iso_country.alpha_2)
 
     def get_image_queries(self):
         words = self.name.split()
         stems = [[w[:i] for i in range(len(w), 2, -1)] for w in words]
-        queries = [*words, self.name, *sum(stems, [])]
-        return [f'{query} music' for query in queries]
-
-    async def fetch_image(self, width=None, height=None):
-        image = self.image(width, height)
-        if image:
-            return image
-
-        queries = self.get_image_queries()
-        photo = None
-        for query in queries:
-            photos = await Unsplash.photo.random(query=query, orientation='squarish')
-            if photos:
-                photo = photos[0]
-                break
-        else:
-            photo = await Unsplash.photo.random(query=query, orientation='squarish')[0]
-
-        if photo is None:
-            return None
-
-        ratio = photo.height / photo.width
-        params = {
-            'genre': self,
-            'color': photo.color,
-            'unsplash_user_fullname': photo.user.name,
-            'unsplash_user_username': photo.user.username,
-        }
-        Image(url=photo.urls.full, width=photo.width, height=photo.height, **params)
-        Image(url=photo.urls.regular, width=Image.REGULAR, height=int(round(ratio * Image.REGULAR)), **params)
-        Image(url=photo.urls.small, width=Image.SMALL, height=int(round(ratio * Image.SMALL)), **params)
-        Image(url=photo.urls.thumb, width=Image.THUMB, height=int(round(ratio * Image.THUMB)), **params)
-        return self.image(width, height)
+        queries = [self.name, *words, *sum(stems, [])]
+        return queries
 
 
-class Playlist(db.Entity):
+class City(db.Entity, ImageMixin):
+    _table_ = 'cities'
+
+    name = PrimaryKey(str)
+    country = Required(Country)
+    playlists = Set('Playlist')
+    fans = Set(User, reverse='top_cities', table='city_fans')
+    haters = Set(User, reverse='disliked_cities', table='city_haters')
+    images = Set(Image, cascade_delete=True)
+
+    def get_image_queries(self):
+        words = self.name.split()
+        stems = [[w[:i] for i in range(len(w), 2, -1)] for w in words]
+        queries = [self.name, self.country.name, *words, *sum(stems, [])]
+        return queries
+
+
+class Playlist(db.Entity, ImageMixin):
     _table_ = 'playlists'
+
     YEAR = '(?P<year>[0-9]{4})'
     GENRE = '(?P<genre>.+)'
     CITY = '(?P<city>.+)'
@@ -242,13 +331,15 @@ class Playlist(db.Entity):
     GENRE_POPULARITY_LOWER = '(?P<popularity>sound|pulse|edge)'
     NEEDLE_POPULARITY = '(?P<popularity>Current|Emerging|Underground)'
 
-    PARTICLE_RE = re.compile(f'The {GENRE_POPULARITY_TITLE} of {GENRE}')
-    SOUND_CITY_RE = re.compile(f'The Sound of {CITY} {COUNTRY_CODE}')
-    NEEDLE_RE = re.compile(f'The Needle / {COUNTRY} {DATE}(?: - {NEEDLE_POPULARITY})?')
-    PINE_NEEDLE_RE = re.compile(f'The Pine Needle / {COUNTRY}')
-    YEAR_IN_GENRE_RE = re.compile(f'{YEAR} in {GENRE}')
-    META_GENRE_RE = re.compile(f'Meta{GENRE_POPULARITY_LOWER}: {GENRE}')
-    META_YEAR_IN_GENRE_RE = re.compile(f'Meta{YEAR}: {GENRE}')
+    PATTERNS = {
+        'sound_of_genre': re.compile(f'The {GENRE_POPULARITY_TITLE} of {GENRE}'),
+        'sound_of_city': re.compile(f'The Sound of {CITY} {COUNTRY_CODE}'),
+        'needle': re.compile(f'The Needle / {COUNTRY} {DATE}(?: - {NEEDLE_POPULARITY})?'),
+        'pine_needle': re.compile(f'The Pine Needle / {COUNTRY}'),
+        'year_in_genre': re.compile(f'{YEAR} in {GENRE}'),
+        'meta_genre': re.compile(f'Meta{GENRE_POPULARITY_LOWER}: {GENRE}'),
+        'meta_year_in_genre': re.compile(f'Meta{YEAR}: {GENRE}'),
+    }
 
     class Popularity(IntEnum):
         SOUND = 0
@@ -264,7 +355,6 @@ class Playlist(db.Entity):
 
     id = PrimaryKey(str)  # pylint: disable=redefined-builtin
     collaborative = Required(bool)
-    images = Set(Image, cascade_delete=True)
     name = Required(str)
     description = Optional(str)
     owner = Required(SpotifyUser)
@@ -273,12 +363,12 @@ class Playlist(db.Entity):
     tracks = Required(int)
     popularity = Optional(int, index=True)
     genre = Optional(Genre)
-    country = Optional(str, index=True)
-    country_code = Optional(str, index=True, max_len=2)
-    city = Optional(str, index=True)
+    country = Optional(Country)
+    city = Optional(City)
     date = Optional(date)
     christmas = Optional(bool, index=True)
     meta = Optional(bool, index=True)
+    images = Set(Image, cascade_delete=True)
     composite_key(genre, popularity, meta)
 
     def play(self, client, device=None):
@@ -297,6 +387,48 @@ class Playlist(db.Entity):
         return f'http://open.spotify.com/user/{self.owner.id}/playlist/{self.id}'
 
     @classmethod
+    def get_fields(cls, groups):
+        fields = {}
+        if 'year' in groups:
+            year = groups['year']
+
+            fields['date'] = datetime(int(year), 1, 1)
+            fields['popularity'] = cls.Popularity.YEAR.value
+
+        if 'genre' in groups:
+            genre = groups['genre'].lower()
+
+            fields['genre'] = Genre.get(name=genre) or Genre(name=genre)
+            fields['christmas'] = 'christmas' in genre
+
+        if 'city' in groups and 'country_code' in groups:
+            city = groups['city']
+            country_code = groups['country_code']
+
+            fields['country'] = Country.from_str(code=country_code)
+            fields['city'] = City.get(name=city) or City(name=city, country=fields['country'])
+            fields['popularity'] = cls.Popularity.SOUND.value
+
+        if 'country' in groups:
+            country = groups['country']
+
+            fields['country'] = Country.from_str(name=country)
+
+        if 'date' in groups:
+            _date = groups['date']
+
+            fields['date'] = datetime.strptime(_date, '%Y%m%d').date()
+
+        if 'popularity' in groups:
+            popularity = groups['popularity']
+            if popularity:
+                fields['popularity'] = cls.Popularity[popularity.upper()].value
+            else:
+                fields['popularity'] = cls.Popularity.ALL.value
+
+        return fields
+
+    @classmethod
     def from_dict(cls, playlist):  # pylint: disable=too-many-return-statements,too-many-statements
         owner = (
             SpotifyUser.get(id=playlist.owner.id)
@@ -310,101 +442,34 @@ class Playlist(db.Entity):
             'public': playlist.public,
             'snapshot_id': playlist.snapshot_id,
             'tracks': playlist.tracks.total,
+            'christmas': 'Pine Needle' in playlist.name or 'christmas' in playlist.name.lower(),
+            'meta': playlist.name.startswith('Meta'),
             'images': [Image.get(url=im.url) or Image(**im) for im in playlist.images]
         }
 
-        match = cls.SOUND_CITY_RE.match(playlist.name)
-        if match:
-            city, country_code = match.groups()
-            fields['popularity'] = cls.Popularity.SOUND.value
-            fields['city'] = city
-            fields['country_code'] = country_code
-            return cls(**fields)
-
-        match = cls.PARTICLE_RE.match(playlist.name)
-        if match:
-            popularity, genre = match.groups()
-            genre = genre.lower()
-            fields['popularity'] = cls.Popularity[popularity.upper()].value
-            fields['genre'] = Genre.get(name=genre) or Genre(name=genre)
-            fields['christmas'] = 'christmas' in genre
-            return cls(**fields)
-
-        match = cls.PINE_NEEDLE_RE.match(playlist.name)
-        if match:
-            country = match.groups()[0]
-            fields['popularity'] = cls.Popularity.CURRENT.value
-            fields['country'] = country
-            fields['christmas'] = True
-            return cls(**fields)
-
-        match = cls.NEEDLE_RE.match(playlist.name)
-        if match:
-            country, _date, popularity = match.groups()
-            if popularity:
-                fields['popularity'] = cls.Popularity[popularity.upper()].value
-            else:
-                fields['popularity'] = cls.Popularity.ALL.value
-            fields['country'] = country
-            fields['date'] = datetime.strptime(_date, '%Y%m%d').date()
-            return cls(**fields)
-
-        match = cls.YEAR_IN_GENRE_RE.match(playlist.name)
-        if match:
-            year, genre = match.groups()
-            year = int(year)
-            genre = genre.lower()
-            fields['genre'] = Genre.get(name=genre) or Genre(name=genre)
-            fields['popularity'] = cls.Popularity.YEAR.value
-            fields['date'] = datetime(year, 1, 1)
-            return cls(**fields)
-
-        match = cls.META_GENRE_RE.match(playlist.name)
-        if match:
-            popularity, genre = match.groups()
-            genre = genre.lower()
-            fields['genre'] = Genre.get(name=genre) or Genre(name=genre)
-            fields['popularity'] = cls.Popularity[popularity.upper()].value
-            fields['meta'] = True
-            return cls(**fields)
-
-        match = cls.META_YEAR_IN_GENRE_RE.match(playlist.name)
-        if match:
-            year, genre = match.groups()
-            year = int(year)
-            genre = genre.lower()
-            fields['genre'] = Genre.get(name=genre) or Genre(name=genre)
-            fields['popularity'] = cls.Popularity.YEAR.value
-            fields['date'] = datetime(year, 1, 1)
-            fields['meta'] = True
-            return cls(**fields)
-
-        logger.warning(f'No pattern matches the playlist name: {playlist.name}')
+        for pattern in cls.PATTERNS.values():
+            match = pattern.match(playlist.name)
+            if match:
+                groups = match.groupdict()
+                fields.update(cls.get_fields(groups))
+                break
+        else:
+            logger.warning(f'No pattern matches the playlist: {playlist.name}')
 
         return cls(**fields)
 
-    def image(self, width=None, height=None):
-        if width:
-            image = self.images.select().where(lambda i: i.width >= width).order_by(Image.width).first()
-        elif height:
-            image = self.images.select().where(lambda i: i.height >= height).order_by(Image.height).first()
-        else:
-            image = self.images.select().order_by(desc(Image.width)).first()
 
-        return image
-
-
-class Artist(db.Entity):
+class Artist(db.Entity, ImageMixin):
     _table_ = 'artists'
 
     id = PrimaryKey(str)  # pylint: disable=redefined-builtin
     name = Required(str, index=True)
     followers = Required(int)
     genres = Set(Genre)
-    images = Set(Image, cascade_delete=True)
     fans = Set(User, reverse='top_artists', table='artist_fans')
     haters = Set(User, reverse='disliked_artists', table='artist_haters')
     popularity = Optional(int)
+    images = Set(Image, cascade_delete=True)
 
     def play(self, client, device=None):
         return client.start_playback(artist=self.uri, device=device)
@@ -455,16 +520,6 @@ class Artist(db.Entity):
             images=images,
             popularity=artist.popularity
         )
-
-    def image(self, width=None, height=None):
-        if width:
-            image = self.images.select().where(lambda i: i.width >= width).order_by(Image.width).first()
-        elif height:
-            image = self.images.select().where(lambda i: i.height >= height).order_by(Image.height).first()
-        else:
-            image = self.images.select().order_by(desc(Image.width)).first()
-
-        return image
 
 
 if config.database.filename:
